43303e7fdf025c5d8b5fb8c0dfd83ae05d4736ae
Radhamante <fafel130@gmail.com>
2022-09-13 16:51:24 +0200

    mongo

--- index.ts
+++ index.ts
@@ -3,18 +3,36 @@ import { graphqlHTTP } from 'express-graphql';
 import schema from './schema';
 import defaultQuery from './queries/defaultQuery';
 
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
 var app = express();
 app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema: schema,
-    graphiql: {
-      defaultQuery
-    }
-  })
+    '/graphql',
+    graphqlHTTP({
+        schema: schema,
+        graphiql: {
+            defaultQuery,
+        },
+    })
 );
 app.use('/', (_, res) => {
-  res.redirect('/graphql');
+    res.redirect('/graphql');
 });
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+
+const MONGO_USER = "admin"
+const MONGO_PASSWORD = "admin"
+const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.wsixukh.mongodb.net/?retryWrites=true&w=majority`;
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose
+    .connect(uri, options)
+    .then(() => {
+        app.listen(4000);
+        console.log(
+            'Running a GraphQL API server at http://localhost:4000/graphql'
+        );
+    })
+    .catch((error: any) => {
+        throw error;
+    });
