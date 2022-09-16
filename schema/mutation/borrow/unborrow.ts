8f122ee2b643d517f5ea10830be5dccc9d39b8dc
luc <luc.coujonde@gmail.com>
2022-09-16 10:20:11 +0200

    unborrow

--- schema/mutation/borrow/unborrow.ts
+++ schema/mutation/borrow/unborrow.ts
@@ -1,11 +1,24 @@
+import { GraphQLNonNull, GraphQLString } from 'graphql';
 import { mutationWithClientMutationId } from 'graphql-relay';
+import History from '../../types/History';
 import MongoUser from '../../mongo/MongoUser';
+import MongoHistory from '../../mongo/MongoHistory';
+import Book from '../../types/Book';
+import User from '../../types/User';
+import MongoBook from '../../mongo/MongoBook';
 
 const unborrowMutation = mutationWithClientMutationId({
     name: 'unborrow',
     description: 'unborrow a book by a user',
-    inputFields: {},
-    outputFields: {},
+    inputFields: {
+        book: { type: new GraphQLNonNull(GraphQLString) },
+        user: { type: new GraphQLNonNull(GraphQLString) },
+    },
+    outputFields: {
+        history: { type: new GraphQLNonNull(History) },
+        book: { type: new GraphQLNonNull(Book) },
+        user: { type: new GraphQLNonNull(User) },
+    },
     mutateAndGetPayload: async (input, context: any) => {
         if (!context.logged) {
             throw Error('User not logged');
@@ -13,10 +26,27 @@ const unborrowMutation = mutationWithClientMutationId({
         const session = await MongoUser.startSession();
         session.startTransaction();
         try {
+            const historyToUpdate = await MongoHistory.findOneAndUpdate({book:input.book, user:input.user, endDate:null}, {
+                $set: { endDate: Date.now() },
+            }).exec();
+            console.log(historyToUpdate)
+            // const updatedHistory = await MongoHistory.updateOne({
+            //     endDate: Date.now(),
+            // });
+            const updatedBook = await MongoBook.findByIdAndUpdate( input.book,{
+                $set: { borrower: null},
+            }).exec();
+            console.log(updatedBook)
+            const updateUser = await MongoUser.findByIdAndUpdate( input.user,{
+                $pull: {booksBorrowed: input.book},
+            }).exec();
+            console.log(updateUser)
+            return {history: historyToUpdate, book: updatedBook, user: updateUser}
         } catch (error) {
             console.log('########### error ##########');
             console.log(error);
         }
+    
         await session.commitTransaction();
         session.endSession();
         return null;
